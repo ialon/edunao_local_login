a5e8c45377551cf527d7ae04142de3e4feb93481
Josemaria Bolanos <josemabol@gmail.com>
2024-10-01 16:14:34 -0600

    Added language strings and version file

---
+++ lang/en/local_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Modified files for login and registration
+ *
+ * @package    local_login
+ * @copyright  Josemaria Bolanos <josemabol@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = "Local Login";
---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Version details for Local Login
+ *
+ * @package    local_login
+ * @copyright  Josemaria Bolanos <josemabol@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2024100100;                // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022112806;                // Requires this Moodle version.
+$plugin->component = 'local_login'; // Full name of the plugin (used for diagnostics).
