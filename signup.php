3a0566cae1b3a3ace472bb668b7398207c2079bb
Josemaria Bolanos <josemabol@gmail.com>
2024-10-01 18:14:47 -0600

    Fixed wrong redirection to logout page

--- signup.php
+++ signup.php
@@ -53,7 +53,7 @@ if (isloggedin() and !isguestuser()) {
     // Prevent signing up when already logged in.
     echo $OUTPUT->header();
     echo $OUTPUT->box_start();
-    $logout = new single_button(new moodle_url('/local/login/logout.php',
+    $logout = new single_button(new moodle_url('/login/logout.php',
         array('sesskey' => sesskey(), 'loginpage' => 1)), get_string('logout'), 'post');
     $continue = new single_button(new moodle_url('/'), get_string('cancel'), 'get');
     echo $OUTPUT->confirm(get_string('cannotsignup', 'error', fullname($USER)), $logout, $continue);
